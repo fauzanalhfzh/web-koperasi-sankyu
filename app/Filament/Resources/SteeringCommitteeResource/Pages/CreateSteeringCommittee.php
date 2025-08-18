513433b05366cc0649d2d9eba942852ca3df9e06
fauzanalhfzh <zenbangga@gmail.com>
2025-08-18 14:31:52 +0700

    fix bugs

--- app/Filament/Resources/SteeringCommitteeResource/Pages/CreateSteeringCommittee.php
+++ app/Filament/Resources/SteeringCommitteeResource/Pages/CreateSteeringCommittee.php
@@ -5,10 +5,19 @@ namespace App\Filament\Resources\SteeringCommitteeResource\Pages;
 use App\Filament\Resources\SteeringCommitteeResource;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Support\Facades\Hash;
 
 class CreateSteeringCommittee extends CreateRecord
 {
     protected static string $resource = SteeringCommitteeResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        if (!empty($data['password'])) {
+            $data['password'] = Hash::make($data['password']);
+        }
+        return $data;
+    }
 
     protected function getRedirectUrl(): string
     {
